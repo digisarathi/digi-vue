49836af8a85be88899df44c8308350b2e6696a4c
Prashant Pandit <prashant@digisarathi.com>
2025-05-16 23:35:25 +0530

    added 404 page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+    <v-container class="text-center py-16">
+        <img src="/404.svg" alt="404 Not Found" width="300" class="mb-4" />
+        <div class="d-flex justify-center">
+            <v-btn color="primary" to="/">Go Home</v-btn>
+        </div>
+    </v-container>
+</template>
