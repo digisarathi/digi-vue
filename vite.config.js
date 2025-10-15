326c8c91f1e3ad56954cf03e326bbd80fef7dda5
Prashant Pandit <prashant@digisarathi.com>
2025-10-16 03:07:50 +0530

    trying url fix

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: process.env.NODE_ENV === 'production' ? 'https://digisarathi.com' : '/',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
